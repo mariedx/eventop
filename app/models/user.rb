dfa28cf49ec3223c91c857af499a2d590757c969
MarieDechaux <marie.dechaux@gmail.com>
2020-11-02 14:48:05 +0100

    first heroku try

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  #has_secure_password
+
+  has_many :attendances
+  has_many :events, through: :attendances
+
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
+  validates :encrypted_password, presence: true, length: {minimum: 6}
+  validates :description, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+#create random users
+15.times do 
+  User.create(email: Faker::Name.first_name+Faker::Name.last_name+"@yopmail.com",
+    encrypted_password: Faker::String.random(length: 6..12),
+    description: Faker::TvShows::Community.quotes,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name)
+end
