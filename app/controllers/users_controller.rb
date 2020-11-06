49af719de99a0f2c708b38ff8114a4209544d42c
MarieDechaux <marie.dechaux@gmail.com>
2020-11-06 18:49:20 +0100

    modif profil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,20 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Profil édité !"
+      redirect_to user_path(@user.id)
+    else
+      flash.now[:alert] = "Impossible d'éditer le profil :"
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :description)
+  end
 end
