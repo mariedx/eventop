02a402ae373799a7aeabf82fca60462e8ae9d9f1
MarieDechaux <marie.dechaux@gmail.com>
2020-11-06 19:12:37 +0100

    modif profil

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,7 +2,7 @@ class Attendance < ApplicationRecord
   after_create :attendance_send
 
   belongs_to :event
-  belongs_to :user, class_name: "User"
+  belongs_to :user
 
   def attendance_send
     event = self.event
